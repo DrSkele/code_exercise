096ca77b7a94cca5afc89d5d59b5a15f289d79bb
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-02-03 21:03:01 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 121 ms, Memory: 20,644 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,44 +1,43 @@
-
 import java.util.*;
 import java.io.*;
-
+ 
 class Solution
 {
     static StringBuilder str = new StringBuilder();
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokens;
-		int T = 10;
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        int T = 10;
+         
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
             str.setLength(0);
-            
+             
             tokens = new StringTokenizer(in.readLine());
-			int nodeCnt = Integer.parseInt(tokens.nextToken());
+            int nodeCnt = Integer.parseInt(tokens.nextToken());
             char[] tree = new char[nodeCnt+1];
-            
+             
             for(int i = 0; i < nodeCnt; i++){
-            	tokens = new StringTokenizer(in.readLine());
+                tokens = new StringTokenizer(in.readLine());
                 int idx = Integer.parseInt(tokens.nextToken());
                 char value = tokens.nextToken().charAt(0);
                 tree[idx] = value;
             }
-            
+             
             inorder(tree, 1);
-            
+             
             System.out.println(String.format("#%d %s", test_case, str.toString()));
-		}
-	}
-    
+        }
+    }
+     
     static void inorder(char[] tree, int idx){
-    	
+         
         if(idx >= tree.length) return;
-        
+         
         int left = 2*idx;
         int right = 2*idx+1;
-        
+         
         inorder(tree, left);
         str.append(tree[idx]);
         inorder(tree, right);
