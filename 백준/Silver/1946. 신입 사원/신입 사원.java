5d477be215d28f56e1b417a3f6dd10d590f0549d
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-02-13 20:30:40 +0900

    [Silver I] Title: 신입 사원, Time: 1236 ms, Memory: 303896 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(in.readLine());
+		StringTokenizer tokens;
+		
+		for(int t = 1; t < T+1; t++) {
+			int n = Integer.parseInt(in.readLine()); 
+			
+			int[] scores = new int[n+1];
+			int[] ranks = new int[n+1];
+			
+			for(int i = 0; i < n; i++) {
+				tokens = new StringTokenizer(in.readLine());
+				
+				int score = Integer.parseInt(tokens.nextToken());
+				int rank = Integer.parseInt(tokens.nextToken());
+				
+				scores[score] = rank;
+				ranks[rank] = score;
+			}
+			
+			HashSet<Integer> set = new HashSet<>();
+
+			set.add(scores[1]);
+			int prevScore = scores[1];
+			
+			for(int i = 2; i < n+1; i++) {
+				if(scores[i] > prevScore) continue;
+				
+				set.add(scores[i]);
+				prevScore = scores[i];
+			}
+			
+			set.add(1);
+			int prevRank = ranks[1];
+			for(int i = 2; i < n+1; i++) {
+				if(ranks[i] > prevRank) continue;
+				
+				set.add(i);
+				prevRank = ranks[i];
+			}
+			
+			System.out.println(set.size());
+		}
+	}
+}
