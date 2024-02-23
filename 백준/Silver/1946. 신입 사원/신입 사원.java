9b545a92985e8da498f8fae83113f3dfcfe6b3d3
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-02-23 21:13:21 +0900

    [Silver I] Title: 신입 사원, Time: 992 ms, Memory: 301288 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -35,15 +35,6 @@ public class Main {
 				prevScore = scores[i];
 			}
 			
-			set.add(1);
-			int prevRank = ranks[1];
-			for(int i = 2; i < n+1; i++) {
-				if(ranks[i] > prevRank) continue;
-				
-				set.add(i);
-				prevRank = ranks[i];
-			}
-			
 			System.out.println(set.size());
 		}
 	}
