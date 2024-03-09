bb100506ca5275c8a55daecfb96c79058431ee32
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-03-09 22:04:33 +0900

    [Gold V] Title: 자두나무, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,75 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
-		init(in);
-		
-		solve();
-	}
-	
-	static int time;
-	static int maxCnt;
-	static ArrayList<int[]> plums;
-	static int[][] dp; //idx, move
-	static void init(BufferedReader in) throws IOException{
-		StringTokenizer tokens = new StringTokenizer(in.readLine());
-		time = Integer.parseInt(tokens.nextToken());
-		maxCnt = Integer.parseInt(tokens.nextToken());
-		plums = new ArrayList<>();
-		//first
-		int prev = Integer.parseInt(in.readLine());
-		int cnt = 1;
-		//after first
-		for(int i = 1; i < time; i++) {
-			int cur = Integer.parseInt(in.readLine());
-			if(prev == cur) {
-				cnt++;
-			} else {
-				plums.add(new int[] {prev, cnt}); // tree , number of plums
-				prev = cur;
-				cnt = 1;
-			}
-		}
-		plums.add(new int[] {prev, cnt});
-		dp = new int[plums.size()][maxCnt+1];
-	}
-	
-	static void solve() {
-		
-		int state = 1;
-		int[] first = plums.get(0);
-		
-		dp[0][0] = first[0] == state ? first[1] : 0;
-		dp[0][1] = first[0] != state ? first[1] : 0;
-		
-		
-		int max = first[1];
-		for(int i = 1; i < plums.size(); i++) {
-			int maxMove = i + 1 <= maxCnt ? i + 1 : maxCnt;
-			int[] cur = plums.get(i);
-			int curTree = cur[0];
-			int curPlum = cur[1];
-			for(int m = 0; m <= maxMove; m++) {
-				if(m == 0) {
-					if(i == 1) {
-						dp[1][0] = state == curTree ? curPlum : 0;
-					} else {
-						dp[i][0] = dp[i-2][0] + curPlum;
-					}
-				} else {
-					int curState = m % 2 == 0 ? 1 : 2;
-					dp[i][m] = Math.max(dp[i-1][m-1], dp[i-1][m])+ (curState == curTree ? curPlum : 0);
-				}
-				
-				max = Math.max(max, dp[i][m]);
-			}
-		}
-		System.out.println(max);
-	}
+class Main {
+    static int stoi(String s) { return Integer.parseInt(s);}
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int t = stoi(st.nextToken());
+        int w = stoi(st.nextToken());
+ 
+        // dp[시간][이동횟수]
+        int[][] dp = new int[t+1][w+1];
+ 
+        for(int i=1; i<=t; i++) {
+            int tree = stoi(br.readLine());
+ 
+            for(int j=0; j<=w; j++) {
+                // 움직임이 없는 경우
+                // 나무 1에 떨어지면 1 추가 / 나무 2에 떨어지면 아무일없음
+                if(j == 0) {
+                    if(tree == 1) 
+                        dp[i][j] = dp[i-1][j] + 1;
+                    else            
+                        dp[i][j] = dp[i-1][j];
+ 
+                    continue;
+                }
+ 
+                // 짝수번 움직이면 나무 1
+                if(j%2 == 0) {
+                    if(tree == 1) 
+                        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j] + 1);
+                    else 
+                        dp[i][j] = max(dp[i-1][j-1] + 1, dp[i-1][j]);
+                }
+                
+                // 홀수번 움직이면 나무 2
+                else {
+                    if(tree == 1)
+                        dp[i][j] = max(dp[i-1][j-1] + 1, dp[i-1][j]);
+                    else
+                        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j] + 1);
+                }
+            }
+        }
+ 
+        int ans = 0;
+        for(int i=0; i<=w; i++)
+            ans = max(ans, dp[t][i]);
+ 
+        System.out.println(ans);
+    }
+ 
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
 }
-
-
