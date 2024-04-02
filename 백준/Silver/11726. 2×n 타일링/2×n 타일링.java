755d58206bbbd289b88b6ac143af2f1aa0839cfc
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-04-02 23:14:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		init(in);
+		
+		solve();
+	}
+	
+	static final int mod = 10_007;
+	static int N;
+	static int[] dp;
+	static void init(BufferedReader in) throws IOException{
+		N = Integer.parseInt(in.readLine());
+		dp = new int[N+1];
+		
+		dp[1] = 1;
+		if(N > 1) dp[2] = 2;
+	}
+	
+	static void solve() {
+		for(int i = 3; i <= N; i++) {
+			dp[i] = dp[i-1]%mod + dp[i-2]%mod;
+		}
+		System.out.println(dp[N]%mod);
+	}
+	
+}
+
+
