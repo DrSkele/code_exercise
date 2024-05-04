8bc8ac790991153810faa57df380da40f5f99ca0
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-05-04 11:39:20 +0900

    [Gold IV] Title: 트리의 지름, Time: 2408 ms, Memory: 127428 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,52 +1,69 @@
 import java.io.*;
 import java.util.*;
 
+// 무방향 그래프
+// BFS 모든 지점
 
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	init();
+    	
+    	solve();  
+    }
+
     static int N;
-    static List<Node> nodes[];
+    static List<Edge>[] map;
     static boolean[] visited;
     static int max;
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        nodes = new ArrayList[N+1];
-        for(int i = 1; i <= N; i++)nodes[i] = new ArrayList<>();
-
-        for(int i = 1; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            nodes[from].add(new Node(to,value));
-            nodes[to].add(new Node(from,value));
-        }
-
-        for(int i = 1; i <= N; i++){
-            visited = new boolean[N+1];
-            visited[i] = true;
-            dfs(i,0);
-        }
-        System.out.println(max);
+    static void init() throws IOException {
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	N = Integer.parseInt(in.readLine());
+    	map = new ArrayList[N+1];
+    	for(int i = 0; i < N+1; i++) {
+    		map[i] = new ArrayList<Edge>();
+    	}
+    	
+    	visited = new boolean[N+1];
+    	for(int i = 0; i < N-1; i++) {
+    		StringTokenizer tokens = new StringTokenizer(in.readLine());
+    		
+    		int from = Integer.parseInt(tokens.nextToken());
+    		int to = Integer.parseInt(tokens.nextToken());
+    		int cost = Integer.parseInt(tokens.nextToken());
+    		
+    		map[from].add(new Edge(to, cost));
+    		map[to].add(new Edge(from, cost));
+    	}
+    	
+    	max = 0;
     }
 
-    public static void dfs(int num, int value){
-        for(Node node : nodes[num]){
-            if(!visited[node.link]){
-                visited[node.link] = true;
-                dfs(node.link, value + node.value);
-            }
-        }
-        max = Math.max(max,value);
+    static void solve(){
+    	for(int i = 1; i <= N; i++) {
+    		visited = new boolean[N+1];
+    		visited[i] = true;
+    		dfs(i, 0);
+    	}
+    	System.out.println(max);
     }
-}
-
-class Node{
-    public int link;
-    public int value;
-
-    public Node(int link, int value){
-        this.link = link;
-        this.value =value;
+    
+    static void dfs(int dest, int cost) {
+    	for(Edge next : map[dest]) {
+    		if(visited[next.dest]) continue;
+    		
+    		visited[next.dest] = true;
+    		dfs(next.dest, cost + next.cost);
+    	}
+    	max = Math.max(max, cost);
+    }
+    
+    static class Edge{
+    	int dest;
+    	int cost;
+    	public Edge(int dest, int cost) {
+    		this.dest = dest;
+    		this.cost = cost;
+    	}
     }
 }
