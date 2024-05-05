e6d3fb5d86339f3ce141f6f02eafb6b09735cdc2
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-05-05 15:51:04 +0900

    Update 트리의 지름.java

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,72 +1,72 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//1167. 트리의 지름 
 public class Main {
-	static class Node{
-		int to;
-		int cost; 
-		public Node(int to, int cost) {
-			this.to=to;
-			this.cost=cost;
-		}
-	}
 
-	static int V;
-	static ArrayList<Node> edges[];
-	static boolean visit[];
-	static int candidate;
-	static int answer;
-	static int max;
+    public static void main(String[] args) throws IOException {
+    	init();
+    	
+    	solve();  
+    }
 
-	public static void main(String[] args) throws Exception {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		V=Integer.parseInt(br.readLine());
-		edges = new ArrayList[V+1];
-		visit = new boolean[V+1];
-		
-		for(int i=0; i<=V; i++) {
-			edges[i]= new ArrayList<Node>();
-		}
-		for(int i=0; i<V; i++) {
-			st = new StringTokenizer(br.readLine());
-			int vertex = Integer.parseInt(st.nextToken());
-			while(true) {
-				int next = Integer.parseInt(st.nextToken());
-				if(next==-1)
-					 break;
-				int cost = Integer.parseInt(st.nextToken());
-				edges[vertex].add(new Node(next,cost));
-			}
-		}
-		
+    static int N;
+    static List<Edge>[] map;
+    static boolean[] visited;
+    static int max;
+    static int maxDest;
+    static void init() throws IOException {
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	N = Integer.parseInt(in.readLine());
+    	map = new ArrayList[N+1];
+    	for(int i = 0; i < N+1; i++) {
+    		map[i] = new ArrayList<Edge>();
+    	}
+    	
+    	visited = new boolean[N+1];
+    	for(int i = 0; i < N; i++) {
+    		StringTokenizer tokens = new StringTokenizer(in.readLine());
+    		
+    		int from = Integer.parseInt(tokens.nextToken());
+    		int to;
+    		while((to = Integer.parseInt(tokens.nextToken())) != -1){
+    			int cost = Integer.parseInt(tokens.nextToken());
+    			map[from].add(new Edge(to, cost));
+    		}
+    	}
+    	
+    	max = 0;
+    	maxDest = 0;
+    }
 
-		dfs(1,0); // 임의의 한 지점에서 dfs를 돌려서 이 정점으로 부터 가장 먼 정점 구하기 
-		
-		visit=new boolean[V+1];
-		dfs(candidate, 0);
-		
-		System.out.println(max);
-	}
-	
-	static public void dfs(int v, int len) {
-		if(len>max) {
-			max=len;
-			candidate=v;
-		}
-		visit[v]=true;
-		for(int i=0; i<edges[v].size(); i++) {
-			Node next = edges[v].get(i);
-			if(visit[next.to]==false) {
-				dfs(next.to,len+next.cost);
-			}
-		}
-	}
-	
+    static void solve(){
+    	visited = new boolean[N+1];
+    	dfs(1, 0);
+    	
+    	visited = new boolean[N+1];
+    	dfs(maxDest, 0);
+    	
+    	System.out.println(max);
+    }
+    
+    static void dfs(int dest, int cost) {
+    	if(cost > max) {
+    		max = cost;
+    		maxDest = dest;
+    	}
+    	visited[dest] = true;
+    	for(Edge next : map[dest]) {
+    		if(visited[next.dest]) continue;
+    		
+    		dfs(next.dest, cost + next.cost);
+    	}
+    }
+    
+    static class Edge{
+    	int dest;
+    	int cost;
+    	public Edge(int dest, int cost) {
+    		this.dest = dest;
+    		this.cost = cost;
+    	}
+    }
 }
