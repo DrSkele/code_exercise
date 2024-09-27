6efcf7368bd299654686948eb715368fc03015be
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-09-27 22:43:49 +0900

    [level 2] Title: 프로세스, Time: 3.35 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        ArrayDeque<Process> processes = new ArrayDeque<>();
+        
+        for(int i = 0; i < priorities.length; i++){
+            pq.add(priorities[i]);
+            processes.add(new Process(priorities[i], i == location));
+        }
+        
+        while(!processes.isEmpty()){
+            Process cur = processes.poll();
+            if(cur.priority < pq.peek()) {
+                processes.add(cur);
+            } else {
+                answer++;
+                pq.poll();
+                //System.out.println(cur.priority);
+                if(cur.target) break;
+            }
+        }
+        
+        return answer;
+    }
+    
+    class Process{
+        int priority;
+        boolean target;
+        public Process(int p, boolean t){
+            priority = p;
+            target = t;
+        }
+    }
+}
