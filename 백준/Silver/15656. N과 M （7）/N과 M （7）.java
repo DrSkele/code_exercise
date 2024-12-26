781a7f012c13e3b4c05c7f02ffc1aecdc6cbd49c
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2024-12-26 14:40:07 +0900

    [Silver III] Title: N과 M (7), Time: 2888 ms, Memory: 608584 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		input(in);
+		solve();
+	}
+	
+	static int size;
+	static int length;
+	static int[] arr;
+	static int[] temp;
+	static Set<String> visited;
+	static List<String> answer;
+	static void input(BufferedReader in) throws IOException {
+		StringTokenizer tokens = new StringTokenizer(in.readLine());
+		size = Integer.parseInt(tokens.nextToken());
+		length = Integer.parseInt(tokens.nextToken());
+		arr = new int[size];
+		tokens = new StringTokenizer(in.readLine());
+		for(int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(tokens.nextToken());
+		}
+		temp = new int[length];
+		visited = new HashSet<>();
+		answer = new ArrayList<>();
+	}
+	
+	static void solve() {
+		Arrays.sort(arr);
+		
+		iterate(0, 0);
+		
+		StringBuilder str = new StringBuilder();
+		for(String next : answer) {
+			str.append(next).append("\n");
+		}
+		System.out.println(str.toString());
+	}
+	
+	static void iterate(int idx, int arrSize) {
+		if(arrSize == length) {
+			StringBuilder str = new StringBuilder();
+			for(int i = 0; i < temp.length; i++) {
+				str.append(temp[i]).append(" ");
+			}
+			if(!visited.contains(str.toString())) {
+				visited.add(str.toString());
+				answer.add(str.toString());
+			}
+			return;
+		}
+		if(idx >= size) return;
+		
+		for(int i = 0; i < size; i++) {
+			temp[arrSize] = arr[i];
+			iterate(idx+1, arrSize+1);
+		}
+		iterate(idx+1, arrSize);
+	}
+}
