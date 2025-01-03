3350165a7a727cf7e3c0e487a8eba12f9a3c6533
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-01-03 10:20:56 +0900

    [Gold V] Title: 치킨 배달, Time: 116 ms, Memory: 12724 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,61 +2,84 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static int pubCnt;
-    static ArrayList<Coord> pubs;
-    static ArrayList<Coord> home;
-    static Coord[] open;
-    static int minVal;
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokens = new StringTokenizer(in.readLine());
-        int size = Integer.parseInt(tokens.nextToken());
-        pubCnt = Integer.parseInt(tokens.nextToken());
-        pubs = new ArrayList<Coord>();
-        home = new ArrayList<Coord>();
-        open = new Coord[pubCnt];
-        minVal = Integer.MAX_VALUE;
-        
-        int homeId = 0;
-        int pubId = 0;
-        for(int y = 0; y < size; y++){
-            tokens = new StringTokenizer(in.readLine());
-            for(int x = 0; x < size; x++){
-                String tile = tokens.nextToken();
-                if("1".equals(tile)) home.add(new Coord(x, y));
-                else if("2".equals(tile)) pubs.add( new Coord(x, y));
-            }
-        }
-        selectPub(0, 0);
-        
-        System.out.print(minVal);
-    }
-    static void selectPub(int start, int cnt){
-        if(cnt == pubCnt){
-            int distSum = 0;
-            for(Coord dep : home){
-                int dist = Integer.MAX_VALUE;
-                for(Coord des : open){
-                    dist = Math.min(dist, Math.abs(dep.x-des.x) + Math.abs(dep.y-des.y));
-                }
-                distSum += dist;
-                if(distSum > minVal) break;
-            }
-            minVal = Math.min(minVal, distSum);
-        } else {
-            for(int i = start; i < pubs.size(); i++){
-                open[cnt] = pubs.get(i);
-                selectPub(i+1, cnt+1);
-            }
-        }
-    }
-}
-class Coord{
-    public int x;
-    public int y;
-    
-    public Coord(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
+	static StringBuilder str;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		input(in);
+		solve();		
+	}
+	
+	static int size;
+	static int cnt;
+	static List<int[]> chicken;
+	static List<int[]> house;
+	static int[][] dist;
+	static int[] temp;
+	static int min;
+	static void input(BufferedReader in) throws IOException {
+		StringTokenizer tokens = new StringTokenizer(in.readLine());
+		size = Integer.parseInt(tokens.nextToken());
+		cnt = Integer.parseInt(tokens.nextToken());
+		
+		chicken = new ArrayList<>();
+		house = new ArrayList<>();
+		
+		for(int y = 0; y < size; y++) {
+			tokens = new StringTokenizer(in.readLine());
+			for(int x = 0; x < size; x++) {
+				String cur = tokens.nextToken();
+				switch(cur) {
+					case "1" : 
+						house.add(new int[] {y, x});
+						break;
+					case "2" : 
+						chicken.add(new int[] {y, x});
+						break;
+					default: break;
+				}
+			}
+		}
+		min = Integer.MAX_VALUE;
+		
+		dist = new int[house.size()][chicken.size()];
+		for(int i = 0; i < house.size(); i++) {
+			int[] from = house.get(i);
+			
+			for(int j = 0; j < chicken.size(); j++) {
+				int[] to = chicken.get(j);
+				
+				dist[i][j] = Math.abs(from[0] - to[0]) + Math.abs(from[1] - to[1]);
+			}
+		}
+		
+		temp = new int[cnt];
+	}
+	
+	static void solve() {
+		comb(-1, 0);
+		System.out.println(min);
+	}
+	
+	static void comb(int idx, int picked) {
+		if(picked == cnt) {
+			int sum = 0;
+			
+			for(int i = 0; i < house.size(); i++) {
+				int minDist = Integer.MAX_VALUE;
+				for(int j : temp) {
+					minDist = Math.min(minDist, dist[i][j]);
+				}
+				sum += minDist;
+			}
+			
+			min = Math.min(min, sum);
+			return;
+		}
+		
+		for(int i = idx + 1; i < chicken.size(); i++) {
+			temp[picked] = i;
+			comb(i, picked+1);
+		}
+	}
 }
