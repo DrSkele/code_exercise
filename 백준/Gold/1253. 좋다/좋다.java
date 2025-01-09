b76c7e6a7f4673df3fed79943023d51c075904d2
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-01-09 10:54:08 +0900

    [Gold IV] Title: 좋다, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,60 +1,57 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
+class Main{
+	static StringBuilder str;
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		init(in);
-		
-		solve();
+		input(in);
+		solve();		
 	}
 	
-	static int N;
-	static int[] arr;
-	static void init(BufferedReader in) throws IOException{
-		N = Integer.parseInt(in.readLine());
+	static int length;
+	static int[] nums;
+	static void input(BufferedReader in) throws IOException {	
+		length = Integer.parseInt(in.readLine());
+		nums = new int[length];
+		
 		StringTokenizer tokens = new StringTokenizer(in.readLine());
-		arr = new int[N];
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(tokens.nextToken());
+		for(int i = 0; i < length; i++) {
+			nums[i] = Integer.parseInt(tokens.nextToken());
 		}
-		Arrays.sort(arr);
+		
+		Arrays.sort(nums);
 	}
 	
 	static void solve() {
 		int cnt = 0;
-		for(int i = 0; i < N; i++) {
-			if(twoPoint(i, 0, N-1)) cnt++;
+		
+		for(int i = 0; i < length; i++) {
+			if(isGood(nums[i], i)) cnt++;
 		}
+		
 		System.out.println(cnt);
 	}
 	
-	static boolean twoPoint(int idx, int left, int right) {
-		
-		int goal = arr[idx];
+	static boolean isGood(int goal, int idx) {
+		int left = idx == 0 ? idx + 1 : 0;
+		int right = idx == length - 1 ? idx - 1 : length - 1;
 		
 		while(left < right) {
-			int sum = arr[left] + arr[right];
+			int sum = nums[left] + nums[right];
+			
+			if(sum == goal) return true;
+			
 			if(sum < goal) {
-				left += 1;
-			} else if (sum > goal){
-				right -= 1;
+				left++;
+				if(left == idx) left++;
 			} else {
-				if(left == idx) {
-					left += 1;
-					continue;
-				} else if(right == idx) {
-					right -= 1;
-					continue;
-				} else {					
-					return true;
-				}
+				right--;
+				if(right == idx) right--;
 			}
 		}
+		
 		return false;
 	}
 }
-
-
