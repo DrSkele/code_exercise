4416982e10cac35827ee7843c72063c467b8199e
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-02-06 19:10:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 100 ms, Memory: 12020 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,63 +3,47 @@ import java.util.*;
 
 public class Main {
 
-	static int N;
-	static int weight;
-	static Item[] items;
-	static int[] dp;
+	static StringBuilder str;
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		init(in);
 		
-		solve();
-		
+		solve(in);			
 	}
-
-	static void init(BufferedReader in) throws IOException {
+	
+	static int nThings;
+	static int maxWeight;
+	static int[] weight;
+	static int[] value;
+	public static void init(BufferedReader in) throws IOException{
 		StringTokenizer tokens = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(tokens.nextToken());
-		weight = Integer.parseInt(tokens.nextToken());
-		items = new Item[N];
-		dp = new int[weight+1];
-		for(int i = 0; i < N; i++) {
-			tokens = new StringTokenizer(in.readLine());
-			items[i] = new Item(Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken()));
-		}
-		Arrays.sort(items);
-	}
-	
-	static void solve() {
-		dp();
-		System.out.println(dp[weight]);
-	}
-	
-	static void dp() {
 		
-		for(int n = 0; n < N; n++) {
-			Item item = items[n];
-			for(int w = weight; w >= item.weight; w--) {
-				dp[w] = Math.max(dp[w], dp[w-item.weight] + item.value);
+		nThings = Integer.parseInt(tokens.nextToken());
+		maxWeight = Integer.parseInt(tokens.nextToken());
+		
+		weight = new int[nThings];
+		value = new int[nThings];
+		
+		for(int i = 0; i < nThings; i++) {
+			tokens = new StringTokenizer(in.readLine());
+			weight[i] = Integer.parseInt(tokens.nextToken());
+			value[i] = Integer.parseInt(tokens.nextToken());
+		}
+	}
+	
+	public static void solve(BufferedReader in) throws IOException {
+		
+		int[] dp = new int[maxWeight+1];
+		
+		for(int i = 0; i < nThings; i++) {
+			int curWeight = weight[i];
+			int curVal = value[i];
+			for(int j = maxWeight; j >= curWeight; j--) {
+				dp[j] = Math.max(dp[j], dp[j-curWeight] + curVal);									
 			}
 		}
-	}
-	
-	static class Item implements Comparable<Item>{
-		public int weight;
-		public int value;
-		public Item(int weight, int value) {
-			this.weight = weight;
-			this.value = value;
-		}
 		
-		@Override
-		public int compareTo(Item i) {
-			int result = 0;
-			if(weight < i.weight) result = -1;
-			else if(weight > i.weight) result = 1;
-			else if(value > i.value) result = -1;
-			else if(value < i.value) result = 1;
-			return result;
-		}
+		System.out.println(dp[maxWeight]);
 	}
 }
