b1a0252e230dbf12df036ba99dc8299226f7605d
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-02-22 16:32:17 +0900

    [Gold I] Title: 그림 교환, Time: 92 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Gold/1029. 그림 교환/그림 교환.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		input(in);
+		solve();
+	}
+	
+	static int num;
+	static int[][] matrix;
+	static int[][] maxCnt;
+	static void input(BufferedReader in) throws IOException {
+		num = Integer.parseInt(in.readLine());
+		matrix = new int[num][num];
+		
+		for(int i = 0; i < num; i++) {
+			char[] line = in.readLine().toCharArray();
+			for(int j = 0; j < num; j++) {
+				matrix[i][j] = Character.getNumericValue(line[j]);
+			}
+		}
+		
+		maxCnt = new int[num][1 << num];
+	}
+	
+	static void solve() {
+		System.out.println(trade(0, 1, 0) + 1);
+	}
+	
+	static int trade(int owner, int visited, int price) {
+		
+		int result = maxCnt[owner][visited];
+		if(result > 0) return result;
+		
+		boolean sold = false;
+		
+		for(int i = 0; i < num; i++) {
+			if(i == owner || matrix[owner][i] < price || (visited & (1 << i)) > 0) continue;
+			
+			result = Math.max(result, trade(i, visited | (1 << i), matrix[owner][i]) + 1);
+			sold = true;
+		}
+		
+		maxCnt[owner][visited] = result;
+		
+		return result;
+	}
+}
