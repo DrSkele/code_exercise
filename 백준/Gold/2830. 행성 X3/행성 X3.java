f7824174f0495f3541516dda89184a34feb3fbaf
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-02-23 15:04:37 +0900

    [Gold III] Title: 행성 X3, Time: 376 ms, Memory: 70012 KB -BaekjoonHub

---
+++ 백준/Gold/2830. 행성 X3/행성 X3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		input(in);
+		solve();
+	}
+	
+	static int cnt;
+	static long[] bit;
+	static void input(BufferedReader in) throws IOException {
+		cnt = Integer.parseInt(in.readLine());
+		bit = new long[20];
+		
+		for(int i = 0; i < cnt; i++) {
+			int citizen = Integer.parseInt(in.readLine());
+			for(int j = 0; j < 20; j++) {
+				if((citizen & (1 << j)) == (1 << j)) bit[j]++;
+			}
+		}
+	}
+	
+	static void solve() {
+		long sum = 0;
+		
+		for(int i = 0; i < 20; i++) {
+			sum += bit[i] * (cnt - bit[i]) * (1L << i); 
+		}
+		
+		System.out.println(sum);
+	}
+}
