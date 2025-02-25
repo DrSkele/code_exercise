29a7048645388405579f1182e34a66eb5b511cd4
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-02-25 17:38:27 +0900

    [Gold III] Title: 소문난 칠공주, Time: 212 ms, Memory: 40472 KB -BaekjoonHub

---
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		input(in);
+		solve();
+	}
+	
+	static char[][] map;
+	static Set<Integer> set;
+	static void input(BufferedReader in) throws IOException {
+		map = new char[5][5];
+		
+		for(int i = 0; i < 5; i++) {
+			map[i] = in.readLine().toCharArray();
+		}
+		
+		set = new HashSet<>();
+	}
+	
+	static void solve() {
+		for(int y = 0; y < 5; y++) {
+			for(int x = 0; x < 5; x++) {
+				boolean isSom = map[y][x] == 'S';
+				chillPrincess((1 << (y * 5 + x)), isSom ? 1 : 0, isSom ? 0 : 1);
+			}
+		}
+		
+		System.out.println(set.size());
+	}
+	
+	static int[] dx = { +1, 0, -1, 0 };
+	static int[] dy = { 0, +1, 0, -1 };
+	static void chillPrincess(int visited, int som, int yeon) {
+		if(som + yeon == 7) {
+			if(!set.contains(visited)) set.add(visited);
+			return;
+		}
+		
+		for(int b = 0; b < 25; b++) {
+			if((visited & (1 << b)) == 0) continue;
+			
+			int x = b % 5;
+			int y = b / 5;
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+				
+				int nextVisit = (1 << (ny * 5 + nx));
+				
+				if((visited & (nextVisit)) == nextVisit) continue;
+				
+				boolean isSom = map[ny][nx] == 'S';
+				
+				if(!isSom && yeon >= 3) continue;
+				
+				chillPrincess(visited | nextVisit, isSom ? som + 1 : som, isSom ? yeon : yeon + 1);
+			}
+		}
+	}
+}
