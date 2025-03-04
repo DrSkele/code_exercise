5f49f796646a0203f8d02ddbdc50d3158eb55f1e
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-03-04 16:36:48 +0900

    [Gold V] Title: Moo 게임, Time: 68 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.java
+++ 백준/Gold/5904. Moo 게임/Moo 게임.java
@@ -1,61 +1,55 @@
-
-import java.io.*;
 import java.util.*;
-
-// boolean array로 moo 값 저장
-// k번째 moo를 배열을 순회하면서 저장
+import java.io.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		init(in);
+		input(in);
+		solve();		
+	}
+	
+	static int idx;
+	static void input(BufferedReader in) throws IOException {
+		idx = Integer.parseInt(in.readLine());
+	}
+	
+	static void solve(){
+		int k = 0;
+		ArrayList<Integer> moo = new ArrayList<>();
+		moo.add(3);
 		
-		solve();
-	}
-	
-	static int N;
-	static int[] moo;
-	static void init(BufferedReader in) throws IOException{
-		N = Integer.parseInt(in.readLine());
-		moo = new int[28];
-	}
-	
-	static void solve() {
-		int length = 3;
-		int cnt = 0;
-		int sum = length;
-		moo[0] = length;
-		while(sum < N) {
-			cnt++;
-			length++;
-			sum += length + sum;
-			moo[cnt] = sum;
+		while(moo.get(moo.size()-1) < idx) {
+			k++;
+			moo.add(moo.get(moo.size()-1) * 2 + k + 3);
 		}
-		//System.out.println(cnt);
-		System.out.print(find(N, cnt));
-	}
-	
-	static String find(int val, int cnt) {
-		if(cnt == 0) {
-			if(val == 1) return "m";
-			else return "o";
-		}
-		if(val <= moo[cnt-1] + cnt) {
-			int newVal = val-moo[cnt-1];
-			if(newVal == 1) return "m";
-			else return "o";
-		} else {
-			int newVal = val - moo[cnt-1] - (cnt+3);
-			int n = 0;
-			for(; n < cnt; n++) {
-				if(newVal <= moo[n]) break;
+		
+		char answer = 'm';
+		
+		while(k >= 0) {
+			if(idx == 1) {
+				answer = 'm';
+				break;
+			} else if(idx <= 3) {
+				answer = 'o';
+				break;
 			}
-			//System.out.println(newVal);
-			return find(newVal, n);
+			
+			if(idx <= moo.get(k-1)) k--;
+			else if(idx <= moo.get(k-1) + k + 3) {
+				idx -= moo.get(k-1);
+				if(idx == 1) {
+					answer = 'm';
+				} else {
+					answer = 'o';
+				}
+				break;
+			} else {
+				idx -= moo.get(k-1) + k + 3;
+				k--;
+			}
 		}
+		
+		System.out.println(answer);
 	}
 }
-
-
