41555585ab8111d7cf24558fae8dc24a6a4cfefb
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-03-04 23:15:49 +0900

    [Gold IV] Title: 행렬 제곱, Time: 64 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -1,70 +1,83 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	// 행렬 제곱 = A*A*A = (A*A)*A
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		input(in);
+		solve();		
+	}
 	
-    public static void main(String[] args) throws IOException {
-    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	init(in);
-    	
-    	solve();  
-    }
-
-    static int size;
-    static long pow;
-    static Map<Long, int[][]> arr;
-    static void init(BufferedReader in) throws IOException {
-    	StringTokenizer tokens = new StringTokenizer(in.readLine());
-    	size = Integer.parseInt(tokens.nextToken());
-    	pow = Long.parseLong(tokens.nextToken());
-    	arr = new HashMap<>();
-    	arr.put(1L, new int[size][size]);
-    	for(int i = 0; i < size; i++) {
-    		tokens = new StringTokenizer(in.readLine());
-    		for(int j = 0; j < size; j++) {
-    			arr.get(1L)[i][j] = Integer.parseInt(tokens.nextToken());
-    		}
-    	}
-    }
-
-    static void solve(){
-    	int[][] ans = powOf(pow);
-    	
-    	StringBuilder str = new StringBuilder();
-    	for(int i = 0; i < size; i++) {
-    		for(int j = 0; j < size; j++) {
-    			str.append(ans[i][j]%1000).append(" ");
-    		}
-    		str.append("\n");
-    	}
-    	
-    	System.out.println(str.toString());
-    }
-    
-    static int[][] powOf(long num) {
-    	if(arr.containsKey(num)) return arr.get(num);
-    	
-    	if(num % 2 == 0) {
-    		return mul(num, powOf(num/2), powOf(num/2));
-    	} else {
-    		return mul(num, mul(num-1, powOf(num/2), powOf(num/2)), arr.get(1L));
-    	}
-    }
-    
-    static int[][] mul(long num, int[][] arr1, int[][] arr2) {
-    	arr.put(num, new int[size][size]);
-    	for(int i = 0; i < size; i++) {
-    		for(int j = 0; j < size; j++) {
-    			int sum = 0;
-    			for(int k = 0; k < size; k++) {
-    				sum += (arr1[i][k] * arr2[k][j]) % 1000;
-    			}
-    			arr.get(num)[i][j] = sum % 1000;
-    		}
-    	}
-    	return arr.get(num);
-    }
+	static int size;
+	static long pow;
+	static long[][] matrix;
+	static Map<Long, long[][]> memo;
+	static void input(BufferedReader in) throws IOException {
+		StringTokenizer tokens = new StringTokenizer(in.readLine());
+		size = Integer.parseInt(tokens.nextToken());
+		pow = Long.parseLong(tokens.nextToken());
+		
+		matrix = new long[size][size];
+		
+		for(int i = 0; i < size; i++) {
+			tokens = new StringTokenizer(in.readLine());
+			for(int j = 0; j < size; j++) {
+				matrix[i][j] = Long.parseLong(tokens.nextToken());
+			}
+		}
+		
+		memo = new HashMap<>();
+		
+		memo.put(0L, matrix);
+		memo.put(1L, matrix);
+	}
+	
+	static void solve(){
+		long[][] answer = power(pow);
+		
+		StringBuilder str = new StringBuilder();
+		
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				str.append(answer[i][j]%1_000).append(" ");
+			}
+			str.append("\n");
+		}
+		
+		System.out.println(str.toString());
+	}
+	
+	static long[][] power(long pow) {
+		if(memo.containsKey(pow)) {
+			return memo.get(pow);
+		}
+		
+		long[][] result;
+		if(pow % 2 == 1) {
+			result = multiply(multiply(power(pow/2), power(pow/2)), power(1));
+		} else {
+			result = multiply(power(pow/2), power(pow/2));
+		}
+		
+		memo.put(pow, result);
+		return result;
+	}
+	
+	static long[][] multiply(long[][] a, long[][] b) {
+		long[][] temp = new long[size][size];
+		
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				int sum = 0;
+				for(int k = 0; k < size; k++) {
+					sum += (a[i][k] * b[k][j]) % 1_000;
+				}
+				
+				temp[i][j] = sum % 1_000;
+			}
+		}
+		
+		return temp;
+	}
 }
