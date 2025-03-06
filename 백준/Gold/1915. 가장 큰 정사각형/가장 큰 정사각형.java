856146ed21c0229d98aeb21443df9e2b06341c7f
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-03-06 09:29:22 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 208 ms, Memory: 23928 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -4,47 +4,47 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
 		input(in);
-		solve();
+		solve();		
 	}
 	
 	static int height;
 	static int width;
-	static int[][] matrix;
+	static int[][] dp;
 	static void input(BufferedReader in) throws IOException {
 		StringTokenizer tokens = new StringTokenizer(in.readLine());
+		
 		height = Integer.parseInt(tokens.nextToken());
 		width = Integer.parseInt(tokens.nextToken());
 		
-		matrix = new int[height][width];
+		dp = new int[height+1][width+1];
 		
-		for(int y = 0; y < height; y++) {
+		for(int i = 1; i < height+1; i++) {
 			char[] line = in.readLine().toCharArray();
-			for(int x = 0; x < width; x++) {
-				matrix[y][x] = Character.getNumericValue(line[x]);
+			for(int j = 1; j < width+1; j++) {
+				if(line[j-1] == '0') continue;
+				
+				int min = Integer.MAX_VALUE;
+				
+				min = Math.min(min, dp[i-1][j]);
+				min = Math.min(min, dp[i][j-1]);
+				min = Math.min(min, dp[i-1][j-1]);
+				
+				dp[i][j] = min + 1;
 			}
 		}
 	}
 	
-	static void solve() {
+	static void solve(){
 		int max = 0;
 		
-		int[][] dp = new int[height][width];
-		
-		for(int y = 0; y < height; y++) {
-			for(int x = 0; x < width; x++) {
-				if(matrix[y][x] == 0) continue;
-				
-				if(y > 0 && x > 0) {
-					int min = matrix[y-1][x-1] == 1 ? dp[y-1][x-1] : 0;
-					min = Math.min(min, matrix[y-1][x-1] == 1 ? dp[y][x-1] : 0);
-					min = Math.min(min, matrix[y-1][x-1] == 1 ? dp[y-1][x] : 0);
-					dp[y][x] = min + 1;
-				} else {
-					dp[y][x] = 1;
-				}
-				max = Math.max(max, dp[y][x]);
+		for(int i = 1; i < height+1; i++) {
+			for(int j = 1; j < width+1; j++) {
+				//System.out.print(dp[i][j]);
+				max = Math.max(max, dp[i][j]);
 			}
+			//System.out.println();
 		}
 		
 		System.out.println(max * max);
