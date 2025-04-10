fd58ba3a08307be5d1158ba07da51a1044d7d012
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2025-04-10 14:14:51 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 132 ms, Memory: 13308 KB -BaekjoonHub

---
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		input(in);
+		solve();	
+	}
+	
+	static int nOrder;
+	static int nBurger;
+	static int nFry;
+	static int[] burgers;
+	static int[] fries;
+	static void input(BufferedReader in) throws IOException {
+		StringTokenizer tokens = new StringTokenizer(in.readLine());
+		nOrder = Integer.parseInt(tokens.nextToken());
+		nBurger = Integer.parseInt(tokens.nextToken());
+		nFry = Integer.parseInt(tokens.nextToken());
+		
+		burgers = new int[nOrder];
+		fries = new int[nOrder];
+		
+		for(int i = 0; i < nOrder; i++) {
+			tokens = new StringTokenizer(in.readLine());
+			burgers[i] = Integer.parseInt(tokens.nextToken());
+			fries[i] = Integer.parseInt(tokens.nextToken());
+		}
+	}
+	
+	static void solve(){
+		int length = nBurger*1000 + nFry + 1;
+		int[] dp = new int[length];
+		Arrays.fill(dp, -1);
+		
+		dp[0] = 0;
+		
+		for(int i = 0; i < nOrder; i++) {
+			for(int b = nBurger; b >= burgers[i]; b--) {
+				for(int f = nFry; f >= fries[i]; f--) {
+					int cur = b*1000 + f;
+					int idx = (b-burgers[i])*1000 + f-fries[i];
+					
+					if(dp[idx] >= 0) dp[cur] = Math.max(dp[cur], dp[idx] + 1);
+				}
+			}
+		}
+		
+		int max = 0;
+		for(int n : dp) {
+			max = Math.max(max, n);
+		}
+		
+		System.out.println(max);
+	}
+	
+}
