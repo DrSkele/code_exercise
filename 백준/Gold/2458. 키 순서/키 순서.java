9fe83060c9641632f0dea7c1513e1a079bf15971
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2026-03-06 12:51:50 +0900

    [Gold IV] Title: 키 순서, Time: 412 ms, Memory: 35140 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -10,60 +10,49 @@ public class Main {
     }
     
     static int students;
-    static ArrayList<Integer>[] bigger;
-    static ArrayList<Integer>[] smaller;
+    static boolean[][] relation;
     static void input(BufferedReader in) throws IOException {
         StringTokenizer tokens = new StringTokenizer(in.readLine());
         students = Integer.parseInt(tokens.nextToken());
         int length = Integer.parseInt(tokens.nextToken());
         
-        bigger = new ArrayList[students+1];
-        smaller = new ArrayList[students+1];
-        
-        for(int i = 0; i < students+1; i++) {
-          bigger[i] = new ArrayList<>();
-          smaller[i] = new ArrayList<>();
-        }
+        relation = new boolean[students+1][students+1];
         
         for(int i = 0; i < length; i++) {
             tokens = new StringTokenizer(in.readLine());
             int small = Integer.parseInt(tokens.nextToken());
             int big = Integer.parseInt(tokens.nextToken());
             
-            smaller[big].add(small);
-            bigger[small].add(big);
+            relation[small][big] = true;
         }
     }
     
-    static boolean[] visited;
     static void solve() {
-        visited = new boolean[students+1];
+        
+        for(int k = 1; k <= students; k++) {
+          for(int i = 1; i <= students; i++) {
+            for(int j = 1; j <= students; j++) {
+              if(relation[i][k] && relation[k][j]) {
+                relation[i][j] = true;
+              }
+            }
+          }
+        }
         
         int cnt = 0;
         
         for(int i = 1; i <= students; i++) {
-            visited = new boolean[students+1];
-            int small = getElements(i, smaller) - 1;
-            visited = new boolean[students+1];
-            int big = getElements(i, bigger) - 1;
+          int reachable = 0;
+          
+          for(int j = 1; j <= students; j++) {
+            if(i == j) continue;
             
-            // System.out.printf("%d %d %d \n", i, small, big);
-            
-            if(small + big + 1 == students) cnt++;
+            if(relation[i][j] || relation[j][i]) reachable++;
+          }
+          
+          if(reachable == students-1) cnt++;
         }
         
         System.out.println(cnt);
-    }
-    
-    static int getElements(int idx, ArrayList<Integer>[] arr) {
-        visited[idx] = true;
-        
-        int cnt = 1;
-        for(int next : arr[idx]) {
-            if(visited[next]) continue;
-            cnt += getElements(next, arr);
-        }
-        
-        return cnt;
     }
 }
