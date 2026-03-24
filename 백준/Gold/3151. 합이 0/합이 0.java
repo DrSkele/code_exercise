5696b22de54ef0a84770b011682e826670b9043d
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2026-03-24 12:57:20 +0900

    [Gold IV] Title: 합이 0, Time: 592 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -2,62 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int length;
-    static ArrayList<Integer> student;
-
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         
+        input(in);
+        solve();
+    }
+    
+    static int length;
+    static ArrayList<Integer> student;
+    
+    static void input(BufferedReader in) throws IOException {
         length = Integer.parseInt(in.readLine());
+        
         student = new ArrayList<>(length);
         
         StringTokenizer tokens = new StringTokenizer(in.readLine());
-        for (int i = 0; i < length; i++) {
-            student.add(Integer.parseInt(tokens.nextToken()));
+        while(tokens.hasMoreTokens()) {
+            int ability = Integer.parseInt(tokens.nextToken());
+            student.add(ability);
         }
         
         Collections.sort(student);
-        solve();
     }
     
     static void solve() {
         long cnt = 0;
         
+        
         for(int i = 0; i < length - 2; i++) {
-            for(int j = i + 1; j < length - 1; j++) {
+            int left = i + 1;
+            int right = length - 1;
+            
+            while(left < right) {
+                int leftVal = student.get(left);
+                int rightVal = student.get(right);
+                int sum = student.get(i) + leftVal + rightVal;
                 
-                int target = -(student.get(i) + student.get(j));
-                
-                int lowerBound = lowerBound(j + 1, length, target);
-                int upperBound = upperBound(j + 1, length, target);
-                
-                cnt += (upperBound - lowerBound);
+                if(sum == 0) {
+                    if(leftVal == rightVal) {
+                        long n = right - left + 1;
+                        cnt += (n * (n - 1)) / 2;
+                        break;
+                    }
+                    
+                    long leftCount = 1;
+                    long rightCount = 1;
+                    
+                    while(left + 1 < right && student.get(left + 1) == leftVal) {
+                        leftCount++;
+                        left++;
+                    }
+                    
+                    while(right - 1 > left && student.get(right - 1) == rightVal) {
+                        rightCount++;
+                        right--;
+                    }
+                    
+                    cnt += leftCount * rightCount;
+                    
+                    left++;
+                    right--;
+                    
+                } else if(sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
+        
         System.out.println(cnt);
-    }
-    
-    static int lowerBound(int left, int right, int target) {
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(student.get(mid) >= target) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-    
-    static int upperBound(int left, int right, int target) {
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(student.get(mid) > target) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
     }
 }
