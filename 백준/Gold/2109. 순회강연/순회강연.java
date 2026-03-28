8a6347ef7a5607382e455fe4f51c8834ec5aa0b4
Seungsoo Kim <45025840+DrSkele@users.noreply.github.com>
2026-03-28 10:41:02 +0900

    [Gold III] Title: 순회강연, Time: 232 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -2,51 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Lecture {
-        int pay;
-        int day;
-
-        Lecture(int pay, int day) {
-            this.pay = pay;
-            this.day = day;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        input(in);
+        solve();
+    }
+    
+    static int length;
+    static PriorityQueue<Request> queue;
+    static int max;
+    public static void input(BufferedReader in) throws IOException {
+        length = Integer.parseInt(in.readLine());
+        queue = new PriorityQueue<>(new Comparator<Request>() {
+            public int compare(Request first, Request second) {
+                int diff = second.fee - first.fee;
+                if(diff == 0) return first.due - second.due;
+                else return diff;
+            }
+        });
+        max = 0;
+        for(int i = 0; i < length; i++) {
+            StringTokenizer tokens = new StringTokenizer(in.readLine());
+            int fee = Integer.parseInt(tokens.nextToken());
+            int due = Integer.parseInt(tokens.nextToken());
+            
+            queue.add(new Request(due, fee));
+            
+            if(max < due) max = due;
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        Lecture[] lectures = new Lecture[n];
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int pay = Integer.parseInt(st.nextToken());
-            int day = Integer.parseInt(st.nextToken());
-            lectures[i] = new Lecture(pay, day);
-        }
-
-        Arrays.sort(lectures, (a, b) -> {
-            if (a.day == b.day) {
-                return Integer.compare(a.pay, b.pay);
-            }
-            return Integer.compare(a.day, b.day);
-        });
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (Lecture lecture : lectures) {
-            pq.offer(lecture.pay);
-
-            if (pq.size() > lecture.day) {
-                pq.poll();
+    
+    public static void solve() {
+        int sum = 0;
+        
+        boolean[] schedule = new boolean[max+1];
+        
+        while(!queue.isEmpty()) {
+            Request cur = queue.poll();
+            
+            for(int i = cur.due; i > 0; i--) {
+                if(!schedule[i]) {
+                    schedule[i] = true;
+                    sum += cur.fee;
+                    break;
+                }
             }
         }
-
-        long answer = 0;
-        while (!pq.isEmpty()) {
-            answer += pq.poll();
+        
+        System.out.println(sum);
+    }
+    
+    static class Request {
+        int due;
+        int fee;
+        
+        public Request(int d, int f) {
+            due = d;
+            fee = f;
         }
-
-        System.out.println(answer);
     }
 }
